85fb53b803abe3c08d13232369eb14b45dbd5280
kabir-afk <kabirsingh12b@gmail.com>
2024-09-26 09:00:28 +0530

    chore : revised contract deployment from another contract by using property as example

---
+++ contracts/Property.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+struct PropertyInfo {
+    string OwnerName;
+    string Address;
+    uint256 Price;
+}
+
+contract Property {
+    // PropertyInfo public info;
+    PropertyInfo info;
+
+    function details()
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            uint256
+        )
+    {
+        return (info.OwnerName, info.Address, info.Price);
+    }
+
+    function createPropertyInfo(
+        string memory _ownerName,
+        string memory _address,
+        uint256 _price
+    ) public {
+        info = PropertyInfo({
+            OwnerName: _ownerName,
+            Address: _address,
+            Price: _price
+        });
+        details();
+    }
+}
---
+++ contracts/PropertyFactory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "./Property.sol";
+
+contract PropertyFactory {
+    Property[] propertyContractArray;
+
+    function createPropertyContract() public {
+        Property dummyPropertyContract = new Property();
+        propertyContractArray.push(dummyPropertyContract);
+    }
+
+    function storeInDummyProperty(
+        string memory _ownerName,
+        string memory _address,
+        uint256 _price
+    ) public {
+        Property dummyProperty = propertyContractArray[propertyContractArray.length - 1];
+        dummyProperty.createPropertyInfo(_ownerName, _address, _price);
+    }
+
+    function getValuesOfDummyProperty(uint256 _PCAIndexNumber)
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            uint256
+        )
+    {
+        Property dummyProperty = propertyContractArray[_PCAIndexNumber];
+        return dummyProperty.details();
+    }
+}
