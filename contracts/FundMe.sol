31804783327603b988560231eaf9bfd48b105378
kabir-afk <kabirsingh12b@gmail.com>
2024-09-28 10:13:32 +0530

    chapter : payable and revert in smart contract

---
+++ contracts/FundMe.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract FundMe {
+    uint256 public number; 
+    function fund() public payable {
+        number = 5;
+        require(msg.value > 1e18,"Not enough ETH");
+    }
+
+    // function withdraw() public {
+
+    // }
+}
