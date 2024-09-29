dd7ffcc6e7bd271dc3e8c1ed8056147b4c2d01dd
Kabir Singh <kabirsingh12b@gmail.com>
2024-09-30 01:13:53 +0530

    working with interfaces

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,7 +8,16 @@ contract FundMe {
         require(msg.value > 1e18,"Not enough ETH");
     }
 
-    function withdraw() public {
-
+    function getPrice () public  {
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        
     }
+    
+    function getConversionRate () public  {
+        
+    }
+
+    // function withdraw() public {
+
+    // }
 }
