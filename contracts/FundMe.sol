a99d43bd336d3d50e9761b86b5c3026e7d0b14e5
Kabir Singh <kabirsingh12b@gmail.com>
2024-09-30 23:52:45 +0530

    chapter : importing from github/npm

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,21 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    uint256 public number; 
+    uint256 public number;
+
     function fund() public payable {
         number = 5;
-        require(msg.value > 1e18,"Not enough ETH");
+        require(msg.value > 1e18, "Not enough ETH");
     }
 
-    function getPrice () public  {
+    function getPrice() public view returns (uint256) {
         // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
     }
-    
-    function getConversionRate () public  {
-        
-    }
+
+    function getConversionRate() public {}
 
     // function withdraw() public {
 
