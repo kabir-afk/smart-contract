1c456a1abbb657e30ea7ae056d0c75f700c79472
Kabir Singh <kabirsingh12b@gmail.com>
2024-10-08 02:45:26 +0530

    chapter : library

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,31 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
     uint256 public number;
-
+    uint256 public minimumUsd = 5 * 1e18;
     function fund() public payable {
         number = 5;
-        require(msg.value > 1e18, "Not enough ETH");
+        require(msg.value.getConversionRate()) >= minimumUsd, "Not enough ETH");
     }
 
-    function getPrice() public view returns (uint256) {
-        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
-        return uint256(answer * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUSD;
-
-    }
+    
 
     // function withdraw() public {
 
